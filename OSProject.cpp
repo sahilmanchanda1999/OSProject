8eb2cbb2a825a154b7bd8c9df1cb1bfa5a51b648
sahilmanchanda1999 <36594384+sahilmanchanda1999@users.noreply.github.com>
2019-04-21 21:13:52 +0530

    Optimization

--- OSProject.cpp
+++ OSProject.cpp
@@ -1,5 +1,6 @@
-#include<iostream>
+#include<iostream>							
 #include<conio.h>
+#include<stdlib.h>
 using namespace std;
 struct Student
 {
@@ -8,26 +9,38 @@ struct Student
 	bool  resource_question_paper;
 	bool resource_all_resources;
 }s1,s2,s3;
+
 void student_process_one()
 {
+	system("cls");
 	s1.resource_all_resources=1;
 	s1.resource_paper=1;
 	s1.resource_question_paper=1;
-	cout<<"Student Process One Completed"<<endl;
+	cout<<"----------------------------------\n";
+	cout<<"~~Student Process One Completed~~"<<endl;
+	cout<<"----------------------------------\n";
 }	
 void student_process_two()
 {
+	system("cls");
 	s2.resource_all_resources=1;
 	s2.resource_pen=1;
 	s2.resource_question_paper=1;
-	cout<<"Student Process Two Completed"<<endl;
+	cout<<"----------------------------------\n";
+	cout<<"~~Student Process Two Completed~~"<<endl;
+	cout<<"----------------------------------\n";
+	
 }
 void student_process_three()
 {
+	system("cls");
 	s3.resource_all_resources=1;
 	s3.resource_pen=1;
 	s3.resource_paper=1;
-	cout<<"Student Process Three Completed"<<endl;
+	cout<<"----------------------------------\n";
+	cout<<"~~Student Process Three Completed~~"<<endl;
+	cout<<"----------------------------------\n";
+	
 }
 int main()
 {
@@ -49,14 +62,65 @@ int main()
 	
 	do
 	{
+		cout<<endl;
 		int a,b;
 		cout<<"1.Pen"<<endl;
 		cout<<"2.Paper"<<endl;
 		cout<<"3.Question Paper"<<endl;
 		cout<<"Select any two items to be placed on the shared table:"<<endl;
 		cin>>a>>b;	
+		if(a==1 && b==2  && s3.resource_all_resources==1)
+		{
+			system("cls");
+			cout<<"----------------------------------\n";
+			cout<<"***Student 3 has already completed his Assignment***"<<endl;
+			cout<<"----------------------------------\n";
+	
+		}
+		if(a==2 && b==1  && s3.resource_all_resources==1)
+		{
+			system("cls");
+			cout<<"----------------------------------\n";
+			cout<<"***Student 3 has already completed his Assignment***"<<endl;
+			cout<<"----------------------------------\n";
+	
+		}
+		if(a==2 && b==3  && s1.resource_all_resources==1)
+		{
+			system("cls");
+			cout<<"----------------------------------\n";
+			cout<<"***Student 1 has already completed his Assignment***"<<endl;
+			cout<<"----------------------------------\n";
+	
+		}
+		if(a==3 && b==2  && s1.resource_all_resources==1)
+		{
+			system("cls");
+			cout<<"----------------------------------\n";
+			cout<<"***Student 1 has already completed his Assignment***"<<endl;
+			cout<<"----------------------------------\n";
+	
+		}
+		if(a==1 && b==3  && s2.resource_all_resources==1)
+		{
+			system("cls");
+			cout<<"----------------------------------\n";
+			cout<<"***Student 2 has already completed his Assignment***"<<endl;
+			cout<<"----------------------------------\n";
+	
+		}
+		if(a==3 && b==1  && s2.resource_all_resources==1)
+		{
+			
+			system("cls");
+			cout<<"----------------------------------\n";
+			cout<<"***Student 2 has already completed his Assignment***"<<endl;
+			cout<<"----------------------------------\n";
+	
+		}
 		if(a==1 && b==2  && s3.resource_all_resources==0)
 		{
+			
 			student_process_three();
 		}
 		if(a==2 && b==1  && s3.resource_all_resources==0)
@@ -83,7 +147,5 @@ int main()
 	while(s1.resource_all_resources==0||s2.resource_all_resources==0||s3.resource_all_resources==0);
 	cout<<"All Student Processes Completed";
 	getch();
+
 }
-
-
-
